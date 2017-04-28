120f43725fc4e507efbee84de4e21adaffe190dd
evavegh <eva.vegh.93@gmail.com>
2017-04-28 13:16:04 +0200

    DateUtils tests getCalendarWithTime, isWithinRange method tested

--- app/src/test/java/com/ivanmagda/habito/utils/HabitoDateUtilsUnitTest.java
+++ app/src/test/java/com/ivanmagda/habito/utils/HabitoDateUtilsUnitTest.java
@@ -1,12 +1,17 @@
 package com.ivanmagda.habito.utils;
 
+import com.ivanmagda.habito.models.Habit;
+import com.ivanmagda.habito.models.HabitRecord;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
-import static com.ivanmagda.habito.utils.HabitoDateUtils.getCalendarWithTime;
+import static com.ivanmagda.habito.utils.HabitoDateUtils.*;
 
 public class HabitoDateUtilsUnitTest {
 
@@ -16,6 +21,36 @@ public class HabitoDateUtilsUnitTest {
 
         assertNotNull(calendar);
         assertEquals(1L, calendar.getTimeInMillis());
+    }
+
+    @Test
+    public void getIsWithinRangeLongSuccess() {
+        assertTrue(isWithinRange(2L, 1L, 5L));
+    }
+
+    @Test
+    public void getIsWithinRangeLongFail() {
+        assertFalse(isWithinRange(2L, 4L, 5L));
+    }
+
+    @Test
+    public void getIsWithinRangeDateSuccess() {
+
+        Date toCheck = new Date(2L);
+        Date start = new Date(1L);
+        Date end = new Date(5L);
+
+        assertTrue(isWithinRange(toCheck, start, end));
+    }
+
+    @Test
+    public void getIsWithinRangeDateFail() {
+
+        Date toCheck = new Date(2L);
+        Date start = new Date(4L);
+        Date end = new Date(5L);
+
+        assertFalse(isWithinRange(toCheck, start, end));
     }
 
 }
